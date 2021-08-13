a097fe506afcc7f3e5bf12c51c2dde735aaf604b
Muskan Gupta <1218.muskangupta@gmail.com>
2021-08-14 04:07:41 +0530

    Added scrping code to app.py by priya and updated html, flask codes by muskan

--- App.py
+++ App.py
@@ -1,70 +1,110 @@
-# Flask sendmail not showing any errors, but no email received
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 from flask_mail import Mail, Message
-
-app= Flask(__name__)
-
-#reading the train file
-df=pd.read_excel("Static/DatasetsUsed/Train.xlsx")
-y_train=df['Label']
-x_train=df['News']
-
-tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.8)
-tfidf_train=tfidf_vectorizer.fit_transform(x_train)
-
-f1=open("model.pck",'rb')
-model=pickle.load(f1)
-f1.close()
+from goose3 import Goose
 
 
-@app.route('/')
-def home():
-    return render_template('Index.html')
-
-@app.route('/', methods=['GET','POST'])
-def Predict():
-    if request.form.get('content') :
-        if (request.method == 'POST'):
-            x_Test = np.array([request.form.get('content')])
-            tfidf_test = tfidf_vectorizer.transform(x_Test)
-            y_pred = model.predict(tfidf_test)
-            return render_template('Index.html', prediction=y_pred , scroll='detection')
-
-    return render_template('Index.html')
+# creating flask object
+app = Flask(__name__)
 
 
 # configuration of mail
-app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'musk.prat.pri.radha@gmail.com'
-app.config['MAIL_PASSWORD'] = 'FakeNewsWebsite1'
+app.config['MAIL_USERNAME'] = 'senders_mail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_SUPPRESS_SEND'] = False
 app.config['TESTING'] = False
 mail = Mail(app)
 
-@app.route('/', methods=['GET','POST'])
+
+# reading the dataset from excel file
+df = pd.read_excel("Static/DatasetsUsed/Train.xlsx")
+y_train = df['Label']
+x_train = df['News']
+
+#Initialize a Tf-idf Vectorizer
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.8)
+
+#Fitting and transforming training data set
+tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+
+
+# Loading Logistic Regression model
+f1 = open("model.pck", 'rb')
+model = pickle.load(f1)
+f1.close()
+
+
+
+# Function for predicting credibility of news from the news content provided
+def Predict(text):
+    x_Test = np.array([text])
+    tfidf_test = tfidf_vectorizer.transform(x_Test)
+    y_pred = model.predict(tfidf_test)
+    return y_pred
+
+
+# Function for accepting feedback and sending mail
 def feedback():
-    if (request.method == 'POST'):
-        name = request.form.get('Name')
-        phone = request.form.get('Phone')
-        email = request.form.get('Email')
-        Feedback = request.form.get('Feedback')
+    name = request.form.get('Name')
+    phone = request.form.get('Phone')
+    email = request.form.get('Email')
+    Feedback = request.form.get('Feedback')
 
-    msg = Message("Hello",
-                  sender="musk.prat.pri.radha@gmail.com",
-                  recipients=["1234muskangupta@gmail.com"])
+    msg = Message("Feedback for Fake News Detection Website",
+            sender="senders_mail@gmail.com",
+            recipients=["receiver1@gmail.com","receiver2@gmail.com",
+                        "receiver3@gmail.com","receiver4@gmail.com"])
 
-    msg.body = "Hello Flask message sent from Flask-Mail"
+    msg.body = '''Hey developers,
+    A Client just viewed your website. The Details of feedback are:
+                  
+    Name of client: %s
+    Phone number: %s
+    Email address: %s
+    Feedback from client: %s
+    '''%(name,phone,email,Feedback)
+
     mail.send(msg)
 
 
-    return render_template('Index.html', sent='not success')
 
-if __name__== "__main__":
+@app.route('/')
+def home():
+    return render_template('Index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def get_val():
+    if (request.method == 'POST'):
+
+        if request.form["Submit_B"]=="newsContent":
+            text = request.form.get('content')
+            return render_template('Index.html', prediction=Predict(text) , scroll="detection")
+
+        elif request.form["Submit_B"]=="message":
+            feedback();
+            return render_template('Index.html', scroll="contact_us")
+
+        else:
+            g = Goose()
+            userLink = str(request.form.get('NewsUrl'))
+            text = g.extract(url=userLink)
+            if 'timesofindia' not in userLink:
+                scraped_text = text.cleaned_text
+            else:
+                scraped_text = text.title
+
+            return render_template('Index.html', prediction=Predict(scraped_text), scroll='detection')
+
+    return render_template('Index.html')
+
+
+if __name__ == "__main__":
     app.run(debug=True)
